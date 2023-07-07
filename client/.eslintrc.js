d6c9bde2ecdeee4d354f0170b4aaae0b6a8518cb
Bell, Ryan <ryan.bell62101@gmail.com>
2023-07-06 23:28:16 -0500

    updated eslint configuration

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "standard-with-typescript",
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
     "overrides": [
         {
             "env": {
@@ -21,6 +24,9 @@ module.exports = {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
+    "plugins": [
+        "react"
+    ],
     "rules": {
     }
 }
